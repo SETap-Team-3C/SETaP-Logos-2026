fada4222684fb5969b0e74328d6586e64c1fda2c
Lewis Ryan <lr9609914@gmail.com>
2026-01-26 15:54:35 +0000

    add make_e function

--- logo.py
+++ logo.py
@@ -1,7 +1,15 @@
 import turtle # Import the turtle module
 
+def make_e(t,radius=100):
+	t.circle(radius,extent=-270)
+	p=t.pos()
+	t.teleport(p[0]-2*radius,p[1])
+	t.right(90)
+	t.forward(2*radius)
+
 t = turtle.Turtle() 
 
+t.teleport(-200,0)
 t.color("blue")
 t.pensize(3)
 t.right(90)
@@ -15,11 +23,15 @@ t.right(180)
 t.forward(85)
 t.right(90)
 t.forward(85)
+p=t.pos()
 t.right(180)
 t.forward(85)
 t.right(90)
 t.forward(100)
 t.right(90)
 t.forward(85)
+
+t.teleport(p[0]+80,p[1]-35)
+make_e(t,radius=50)
 
 input()
