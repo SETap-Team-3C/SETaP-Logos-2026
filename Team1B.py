d49c05e2cad4c9320fc95178b2b885c637d1b128
Arsenii <136461226+rezorkin@users.noreply.github.com>
2026-01-28 10:50:05 +0000

    team1b Added blue sqare

--- Team1B.py
+++ Team1B.py
@@ -11,6 +11,17 @@ for i in range(3):
     t.right(120)
 
 t.penup()
+t.goto(-35, 60) 
+t.pendown()
+t.color('blue')
+t.pensize(3)
+t.begin_fill()
+for _ in range(4):
+    t.forward(70)
+    t.right(90)
+t.end_fill()
+
+t.penup()
 t.goto(0, -25)
 t.pendown()
 t.color('orange')
